17231059dfa82c26a72c68fbdec2a5128398575d
Travismacmillan <travis.macmillan10@gmail.com>
2017-02-07 11:27:50 -0400

    Quick demo
    test demo

---
+++ ModernUIApp1/ControlList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernUIApp1
+{
+    public class ControlList
+    {
+        private bool isLoop;
+        public bool IsLoop
+        {
+            get { return isLoop; }
+            set
+            {
+                if (value == isLoop) return;
+                isLoop = value;
+            }
+        }
+
+        private string controlLabel;
+        public string ControlLabel
+        {
+            get { return controlLabel; }
+            set
+            {
+                if (value == controlLabel) return;
+                controlLabel = value;
+            }
+        }
+
+        private ObservableCollection<ControlList> childControlList;
+        public ObservableCollection<ControlList> ChildControlList
+        {
+            get { return childControlList; }
+            set
+            {
+                if (value == childControlList) return;
+                childControlList = value;
+            }
+        }
+    }
+}
---
+++ ModernUIApp1/Protocol.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernUIApp1
+{
+    /// <summary>
+    /// Interaction logic for Protocol.xaml
+    /// </summary>
+    public partial class Protocol : UserControl
+    {
+        private ObservableCollection<ControlList> treeControlList = new ObservableCollection<ControlList>();
+        public ObservableCollection<ControlList> TreeControlList
+        {
+            get { return treeControlList; }
+            set
+            {
+                if (value == treeControlList) return;
+                treeControlList = value;
+            }
+        }
+        public Protocol()
+        {
+            
+
+            ControlList child = new ControlList();
+            child.ControlLabel = "child1";
+            child.IsLoop = false;
+
+            ControlList child2 = new ControlList();
+            child2.ControlLabel = "child2";
+            child2.IsLoop = false;
+
+
+
+            ControlList loop2 = new ControlList();
+            loop2.ControlLabel = "Loop2";
+            loop2.IsLoop = true;
+            loop2.ChildControlList = new ObservableCollection<ControlList>();
+            loop2.ChildControlList.Add(child2);
+
+            ControlList loop = new ControlList();
+            loop.ControlLabel = "Loop1";
+            loop.IsLoop = true;
+            loop.ChildControlList = new ObservableCollection<ControlList>();
+            loop.ChildControlList.Add(child);
+            loop.ChildControlList.Add(loop2);
+
+            TreeControlList.Add(loop);
+            DataContext = this;
+            InitializeComponent();
+            
+        }
+    }
+}
